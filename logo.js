c337582e953bc9f311230faf6b66fc347e9c9440
JahshuaMaxwell <jahshua.maxwell@gmail.com>
2026-09-17 10:37:36 -0700

    Add Tide Lantern logo.js injector (fix assets/logo.png 404)

---
+++ logo.js
@@ -0,0 +1,7 @@
+// Tide Lantern logo (embedded)
+(function () {
+  var src = 'data:image/png;base64,PLACEHOLDER_WILL_REPLACE';
+  document.querySelectorAll('img[src="assets/logo.png"]').forEach(function (img) {
+    img.src = src;
+  });
+})();
